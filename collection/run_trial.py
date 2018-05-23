c0d507b1743414b376be29209004b74e0b14b5d8
Zack Johnson <zachary.johnson183@gmail.com>
2018-05-23 17:22:29 -0400

    Wed May 23 17:22:29 EDT 2018

--- collection/run_trial.py
+++ collection/run_trial.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
-import time, os, ctypes, sys, random, subprocess, shutil, getopt, serial
+import time, os, ctypes, sys, random, subprocess, shutil, getopt, serial, argparse
 
 class ssd_test_class:
-    def __init__(self, size, floc, rloc, trials, start, stop, wait_time, trigger_on, trigger_off):
-        self.size           = int(size)
-        self.sname          = str(size)+"MB.txt"
-        self.fname          = floc+'\\'+str(size)+"MB.txt"
+    def __init__(self, size, floc, args, trigger_on, trigger_off):
+        self.size           = 0
+        self.sname          = ""
+        self.fname          = ""
         self.floc           = floc
-        self.rloc           = rloc
-        self.sleep          = int(wait_time)
-        self.start          = start
-        self.stop           = stop
+        self.rloc           = args.ssd_path
+        self.sleep          = int(args.wait)
+        self.start          = args.start_buf
+        self.stop           = args.record_time
         self.trigger_on     = trigger_on
         self.trigger_off    = trigger_off
 
-    def write_test(self):
+    def read_test(self):
 
         # create a file and save it to the C:\ Drive
         if not os.path.isfile(self.fname):
             create_file(self.size, self.fname)
         shutil.move(self.fname, self.rloc+'\\'+self.sname)
+
+        self.clear_cache()
 
         # START WRITE TRIAL
         self.trigger_on.write(bytes(42))
@@ -29,13 +31,15 @@ class ssd_test_class:
         self.trigger_off.write(bytes(42))
         # END OF WRITE TRIAL
 
-        wait()
+        self.wait()
 
-    def read_test(self):
+    def write_test(self):
 
         # create a file and save it the SSD
         if not os.path.isfile(self.fname):
             create_file(self.size, self.fname)
+
+        self.clear_cache()
 
         # START READ TRIAL
         self.start.write(bytes(42))
@@ -45,8 +49,8 @@ class ssd_test_class:
         self.stop.write(bytes(42))
         # END OF READ TRIAL
 
-        move_file(self.rloc+'\\'+self.sname,self.fname)
-        wait()
+        shutil.move(self.rloc+'\\'+self.sname,self.fname)
+        self.wait()
 
     def clear_cache(self):
         os.system(os.getcwd()+'EmptyStandbyList.exe standbylist')
@@ -69,32 +73,49 @@ class ssd_test_class:
             file.close()
             os.remove(file_name)
 
+def ParseArgs():
+    parser = argparse.ArgumentParser(description='automated test script')
+
+    parser.add_argument("-n", "--num_trials",
+                        default=10,
+                        help="number of read and write trials to run")
+
+    parser.add_argument("-l", "--ssd_path",
+                        help="path to target directory on test SSD")
+
+    parser.add_argument("-w", "--wait",
+                        default=60,
+                        help="idle time between trials in seconds")
+
+    parser.add_argument("-p", "--start_buf",
+                        default=2,
+                        help="seconds to begin recording before operation begins")
+
+    parser.add_argument("-t", "--record_time",
+                        default=20,
+                        help="seconds to record after event is triggered")
+
+    parser.add_argument("-s", "--sizes",
+                        type=list,
+                        default=[1024],
+                        help="file sizes to test (in MB)")
+
+    return parser.parse_args()
+
 def main():
 
-    #####################################################################
-    # User Defined Trial Parameters - (safe to edit these)
-    #####################################################################
-    trigger_on   = serial.Serial('COM13',9600)
-    trigger_off  = serial.Serial('COM12',9600)
-    trials  = 10
-    sizes   = [1024,5120]
-    floc    = os.getcwd()
-    rloc    = "C:\\Users\\samsungssd\\Desktop\\"
-    start   = 2
-    stop    = 20
-    wait    = 60
-    #####################################################################
+    trigger_on  = serial.Serial('COM13',9600)
+    trigger_off = serial.Serial('COM12',9600)
+    floc        = os.getcwd()
+    args        = ParseArgs()
 
-    test    = ssd_test_class(0, floc, rloc, trials, start, stop, wait, trigger_on, trigger_off)
+    test    = ssd_test_class(0, os.getcwd(), args, trigger_on, trigger_off)
     for size in sizes:
         test.size = size
         test.sname = str(size)+"MB.txt"
         test.fname = test.floc + '\\' + test.sname
-        test.clear_cache()
         for trial in range(trials):
-            test.clear_cache()
             test.write_test()
-            test.clear_cache()
             test.read_test()
     quit()
 
